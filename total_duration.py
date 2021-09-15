a1da79362621a797f543e9a3c7660641bb03d5d7
Khailas R <khailas303@gmail.com>
2021-09-16 00:08:16 +0530

    Updated

--- total_duration.py
+++ total_duration.py
@@ -48,6 +48,7 @@ def main():
         )
         video_response = video_request.execute()
 
+
         for item in video_response['items']:
             duration = item['contentDetails']['duration']
             
@@ -58,6 +59,7 @@ def main():
             hours = int(hours.group(1)) if hours else 0
             minutes = int(minutes.group(1)) if minutes else 0
             seconds = int(seconds.group(1)) if seconds else 0
+            
             
             video_seconds = timedelta(
                 hours = hours,
