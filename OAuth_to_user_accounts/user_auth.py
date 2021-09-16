4edb747a9396bd9f12597fac0c312922d5d900a8
Khailas R <khailas303@gmail.com>
2021-09-17 01:55:49 +0530

    Modules updated

--- OAuth_to_user_accounts/user_auth.py
+++ OAuth_to_user_accounts/user_auth.py
@@ -2,8 +2,6 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 import pickle
-import sys
-import time
 import os
 
 
